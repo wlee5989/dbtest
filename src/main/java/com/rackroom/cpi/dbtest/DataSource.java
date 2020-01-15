3774422f1be2c2d3afc69cc58f140db951afe885
Wonman Lee <wlee@rackroom.com>
2020-01-15 08:27:19 -0500

    no propery file

--- src/main/java/com/rackroom/cpi/dbtest/DataSource.java
+++ src/main/java/com/rackroom/cpi/dbtest/DataSource.java
@@ -7,16 +7,24 @@ import com.zaxxer.hikari.HikariDataSource;
 
 public class DataSource {
  
-    private static HikariConfig config = new HikariConfig("src/main/resources/datasource.properties");
+    private static HikariConfig config = new HikariConfig();
     private static HikariDataSource ds;
  
+    
+    
     static {
-//        config.setJdbcUrl( "jdbc_url" );
-//        config.setUsername( "database_username" );
-//        config.setPassword( "database_password" );
-//        config.addDataSourceProperty( "cachePrepStmts" , "true" );
-//        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
-//        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+    	
+    	String jdbcUrl="jdbc:mysql:///hybrisdb?cloudSqlInstance=rrs-hybris-dev:us-east4:sql&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=hybris&password=Nd4734MvKjyu4NAT";
+    	String	dataSourceUser="hybris";
+    	String	dataSourcePassword="Nd4734MvKjyu4NAT";
+    	
+    	
+        config.setJdbcUrl( jdbcUrl );
+        config.setUsername( dataSourceUser );
+        config.setPassword( dataSourcePassword );
+        config.addDataSourceProperty( "cachePrepStmts" , "true" );
+        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
         ds = new HikariDataSource( config );
     }
  
