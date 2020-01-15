d44b49d723df52f805195459527ff0b488af5aac
Wonman Lee <wlee@rackroom.com>
2020-01-15 08:03:02 -0500

    added rest api

--- src/main/java/com/rackroom/cpi/dbtest/DbtestApplication.java
+++ src/main/java/com/rackroom/cpi/dbtest/DbtestApplication.java
@@ -8,61 +8,65 @@ import java.sql.SQLException;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DbtestApplication implements CommandLineRunner {
+@RestController
+public class DbtestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbtestApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-      Connection con = null;
-      PreparedStatement pst = null;
-      ResultSet rs = null;
-      
-      try {
+	@RequestMapping("/")
+	public void test() {
+	      Connection con = null;
+	      PreparedStatement pst = null;
+	      ResultSet rs = null;
+	      
+	      try {
 
-          con = DataSource.getConnection();
-          pst = con.prepareStatement("SELECT * FROM Cars");
-          rs = pst.executeQuery();
+	          con = DataSource.getConnection();
+	          pst = con.prepareStatement("SELECT * FROM Cars");
+	          rs = pst.executeQuery();
 
-          while (rs.next()) {
+	          while (rs.next()) {
 
-              System.out.format("%d %s %d %n", rs.getInt(1), rs.getString(2), 
-                      rs.getInt(3));
-          }
+	              System.out.format("%d %s %d %n", rs.getInt(1), rs.getString(2), 
+	                      rs.getInt(3));
+	          }
 
-      } catch (SQLException ex) {
+	      } catch (SQLException ex) {
 
 
-          System.err.println(ex.getMessage());
+	          System.err.println(ex.getMessage());
 
-      } finally {
+	      } finally {
 
-          try {
-          
-              if (rs != null) {
-                  rs.close();
-              }
-              
-              if (pst != null) {
-                  pst.close();
-              }
-              
-              if (con != null) {
-                  con.close();
-              }
-              
-              
+	          try {
+	          
+	              if (rs != null) {
+	                  rs.close();
+	              }
+	              
+	              if (pst != null) {
+	                  pst.close();
+	              }
+	              
+	              if (con != null) {
+	                  con.close();
+	              }
+	              
+	              
 
-          } catch (SQLException ex) {
+	          } catch (SQLException ex) {
 
-              System.err.println(ex.getMessage());
-          }
-      }
+	              System.err.println(ex.getMessage());
+	          }
+	      }
+		
 	}
+
 
 }
